f148d05b7f4d860ff188e1479b0835e8ea57ef30
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-04-01 19:04:30 +0600

    Initial Commit

---
+++ CalculatorDemoIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorDemoIOS
+//
+//  Created by Rustam Keneev on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
